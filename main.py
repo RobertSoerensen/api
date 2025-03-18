6f48335f9cc98edb39740e8f3c1f3986d9ae524b
Robert <robertlls4480@gmail.com>
2025-03-18 15:35:43 +0100

    added inital functionality of '/events'

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI
+import requests
+from datetime import datetime
 
 app = FastAPI()
 
+# Counter Global variables
+calendar_id = ""
+api_key = ""
 
 counterAPI = "/counter/v1"
 @app.get(counterAPI + "/events")
 async def getEvents():
-    return {"message": "Events Success!"}
+    url = f"https://www.googleapis.com/calendar/v3/calendars/{calendar_id}/events"
+    params = {
+        'timeMin': datetime.utcnow().isoformat() + 'Z',  # Ensure it's in UTC format
+        'singleEvents': 'true',
+        'orderBy': 'startTime',
+        'key': api_key
+    }
+    
+    response = requests.get(url, params=params)
+    response.raise_for_status()  # Raise an error for bad responses
+    data = response.json()
+    
+    return data.get('items', [])
